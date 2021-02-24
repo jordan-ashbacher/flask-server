7d7d3ee06e4e51270e6e8467e1be6e439b52421d
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-02-23 21:48:41 -0600

    creates dotenv file and imports python-dotenv

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from pathlib import Path
+from dotenv import load_dotenv
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 import requests
+import os
+
+
+
+KEY = os.getenv("GOOGLE_API_KEY")
+
 app = Flask(__name__)
 
 @app.route('/get', methods=['GET'])
@@ -12,6 +22,6 @@ def postTest():
 
 @app.route('/map')
 def pizza():
-    response = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=44.977753,-93.2650108&radius=32000&type=restaurant&keyword=pizza&key=AIzaSyBsf7l3iI39TwWrf6YbrBZzvNx2YD3662s')
-    return response.text
+    response = requests.get(f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=44.977753,-93.2650108&radius=32000&type=restaurant&keyword=pizza&key={KEY}')
+    return response.json()
   
